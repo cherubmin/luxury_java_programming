80ab673929ffaa2085692735e90036cc4be51abe
cherubmin <cherubjeongmin@gmail.com>
2024-06-06 21:35:09 +0900

    Initial commit

---
+++ src/first/challenge1/MyInfoApp.java
@@ -0,0 +1,18 @@
+/*
+화면에 자신의 이름, 나이, 학과 등의 정보를 출력하는 자바 응용프로그램을 작성하라.
+이클립스를 사용하여 OpenChallenge1이라는 이름의 프로젝트를 만들고, 클래스 이름을 MyInfoApp로 하라.
+그러면 MyInfoApp.java 파일로 자동 저장된다. 프로그램을 실행하면 다음과 같이 출력된다.
+
+kitae Hwang
+20 years old
+Department of Computer Engineering
+ */
+package first.challenge1;
+
+public class MyInfoApp {
+    public static void main(String[] args) {
+        System.out.println("kitae Hwang");
+        System.out.println("20 years old");
+        System.out.println("Department of Computer Engineering");
+    }
+}
---
+++ src/second/challenge1/Sorry.java
@@ -0,0 +1,13 @@
+/*
+화면에 "Sorry" "Thankyou" 를 출력하는 자바 프로그램을 작성하라.
+Workspace는 C:\Temp로 하고 프로젝트 이름은 1-2로 한다. 클래스 이름은 Sorry로 한다.
+ */
+
+package second.challenge1;
+
+public class Sorry {
+    public static void main(String[] args) {
+        System.out.println("Sorry~~");
+        System.out.println("재밌어서 미안합니다~~");
+    }
+}
