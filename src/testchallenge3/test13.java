11179b79294ce1249ef1cae9b26f1f842fb1ecc4
cherubmin <cherubjeongmin@gmail.com>
2024-06-15 18:23:08 +0900

    24-06-15(4)

--- src/testchallenge3/test13.java
+++ src/testchallenge3/test13.java
@@ -6,6 +6,23 @@ package testchallenge3;
 
 public class test13 {
     public static void main(String[] args) {
+        for (int i=0; i < 100; i++) {
+            int lastDigit = i % 10;
+            int firstDigit = i / 10;
+            if (lastDigit == 3 || lastDigit == 6 || lastDigit == 9) {
+                if (firstDigit == 3 || firstDigit == 6 || firstDigit == 9) {
+                    System.out.println(i + " 박수 짝짝");
+                }else System.out.println(i + " 박수 짝");
+            }
 
+            if (firstDigit == 3 || firstDigit == 6 || firstDigit == 9) {
+                if (lastDigit == 0 || lastDigit == 1 || lastDigit == 2 || lastDigit == 4 || lastDigit == 5
+                        || lastDigit == 7 || lastDigit == 8) {
+                    System.out.println(i + " 박수 짝");
+                }
+            }
+
+
+        }
     }
 }
